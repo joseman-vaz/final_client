83b3b95fe834b69aaca76b677a70d4c67442d8c7
Jose Vazquez <vazquezjoseman13@gmail.com>
2023-07-17 21:03:30 +0200

    Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex items-center justify-center w-full h-12 border-t">
-      <div className="flex items-center justify-center text-gray-500">
+    <footer className="flex items-center justify-center w-full h-12 border-t mx-auto">
+      <div className="flex items-center justify-center text-gray-500 mt-2">
         © 2023 Scroll/Ai. All rights reserved. Made with ❤️ by José Vázquez.
       </div>
     </footer>
