d1a61304feae2af5bc76f13796f88b73c2c0dded
Jose Vazquez <vazquezjoseman13@gmail.com>
2023-07-18 21:26:46 +0200

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        xs: "480px",
+        xs: "400px",
       },
       fontFamily: {
         inter: ["Inter var", "sans-serif"],
