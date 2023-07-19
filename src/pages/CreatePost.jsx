1b2458e1fca278ff230512e4f1ad2f5ae998e551
Jose Vazquez <vazquezjoseman13@gmail.com>
2023-07-19 11:16:29 +0200

    Update

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -93,85 +93,82 @@ const CreatePost = () => {
   };
 
   return (
-    <>
+    <div className="max-w-screen-md mx-auto">
       <Navbar />
-      <div className="flex items-center justify-center px-2 sm:px-4">
-        <section className="max-w-7xl mx-auto mt-4">
-          <div>
-            <h1 className="font-extrabold text-[#222328] text-[32px] text-center">
-              Create your masterpiece
-            </h1>
-            <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px] text-center">
-              Let your imagination run wild and create your own masterpiece with
-              the help of AI.
-            </p>
+
+      <section className="mx-auto mt-4">
+        <div>
+          <h1 className="font-extrabold text-[#222328] text-[32px] text-center">
+            Create your masterpiece
+          </h1>
+          <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px] text-center">
+            Let your imagination run wild and create your own masterpiece with
+            the help of AI.
+          </p>
+        </div>
+
+        <form className="mt-10 px-4 sm:px-0" onSubmit={handleSubmit}>
+          <div className="imageCreator flex flex-col gap-5">
+            <FormField
+              labelName="Prompt"
+              type="text"
+              name="prompt"
+              placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
+              value={form.prompt}
+              handleChange={handleChange}
+              isSurpriseMe
+              handleSurpriseMe={handleSurpriseMe}
+            />
+
+            <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-96 max-w-full h-96 max-h-full p-3 flex justify-center mx-auto items-center">
+              {form.photo ? (
+                <img
+                  src={form.photo}
+                  alt={form.prompt}
+                  className="w-full h-full object-contain"
+                />
+              ) : (
+                <img
+                  src={preview}
+                  alt="preview"
+                  className="w-full h-full object-contain opacity-40"
+                />
+              )}
+
+              {generatingImg && (
+                <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                  <Loader />
+                </div>
+              )}
+            </div>
           </div>
 
-          <form
-            className="mt-10 max-w-3xl px-4 sm:px-0"
-            onSubmit={handleSubmit}
-          >
-            <div className="imageCreator flex flex-col gap-5">
-              <FormField
-                labelName="Prompt"
-                type="text"
-                name="prompt"
-                placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
-                value={form.prompt}
-                handleChange={handleChange}
-                isSurpriseMe
-                handleSurpriseMe={handleSurpriseMe}
-              />
+          <div className="mt-5 flex flex-col sm:flex-row justify-between gap-5">
+            <button
+              type="button"
+              onClick={generateImage}
+              className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-2 py-2.5 text-center hover:bg-green-800"
+            >
+              {generatingImg ? "Generating..." : "Generate"}
+            </button>
 
-              <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-96 max-w-full h-96 max-h-full p-3 flex justify-center mx-auto items-center">
-                {form.photo ? (
-                  <img
-                    src={form.photo}
-                    alt={form.prompt}
-                    className="w-full h-full object-contain"
-                  />
-                ) : (
-                  <img
-                    src={preview}
-                    alt="preview"
-                    className="w-full h-full object-contain opacity-40"
-                  />
-                )}
+            <button
+              type="submit"
+              className="mt-2 sm:mt-0 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-2 py-2.5 text-center hover:bg-blue-700"
+            >
+              {loading ? "Sharing..." : "Share with the Community"}
+            </button>
+          </div>
 
-                {generatingImg && (
-                  <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
-                    <Loader />
-                  </div>
-                )}
-              </div>
-            </div>
+          <p className="mt-3 text-[#666e75] text-[14px]">
+            ** Once you have created the image you want, you can share it with
+            others in the community **
+          </p>
+        </form>
+      </section>
 
-            <div className="mt-5 flex flex-col sm:flex-row justify-between gap-5">
-              <button
-                type="button"
-                onClick={generateImage}
-                className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-2 py-2.5 text-center hover:bg-green-800"
-              >
-                {generatingImg ? "Generating..." : "Generate"}
-              </button>
-
-              <button
-                type="submit"
-                className="mt-2 sm:mt-0 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-2 py-2.5 text-center hover:bg-blue-700"
-              >
-                {loading ? "Sharing..." : "Share with the Community"}
-              </button>
-            </div>
-
-            <p className="mt-3 text-[#666e75] text-[14px]">
-              ** Once you have created the image you want, you can share it with
-              others in the community **
-            </p>
-          </form>
-        </section>
-      </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
